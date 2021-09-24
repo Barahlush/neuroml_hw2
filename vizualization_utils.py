f337d1d6a73b0ac0144c46df8d2d9b0e71823a16
Barahlush <baraltiva@gmail.com>
2021-09-24 17:06:23 +0000

    Refactored code, moved vizualization and data preprocessing functions into their own files.

---
+++ vizualization_utils.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import nibabel
+
+def plot_central_cuts(img, title=""):
+    """
+    param image: tensor or np array of shape (CxDxHxW) if t is None
+    """
+    if isinstance(img, torch.Tensor):
+        img = img.numpy()
+        if (len(img.shape) > 3):
+            img = img[0,:,:,:]
+                
+    elif isinstance(img, nibabel.nifti1.Nifti1Image):    
+        img = img.get_fdata()
+   
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(3 * 6, 6))
+    axes[0].imshow(img[ img.shape[0] // 2, :, :])
+    axes[1].imshow(img[ :, img.shape[1] // 2, :])
+    axes[2].imshow(img[ :, :, img.shape[2] // 2])
+    
+    plt.show()
+    
+def plot_predicted(img, seg, delta = 0, title=""):
+    """
+    param image: tensor or np array of shape (CxDxHxW) if t is None
+    """
+    if isinstance(img, torch.Tensor):
+        img = img.cpu().numpy()
+        if (len(img.shape) == 5):
+            img = img[0,0,:,:,:]
+        elif (len(img.shape) == 4):
+            img = img[0,:,:,:]
+                
+    elif isinstance(img, nibabel.nifti1.Nifti1Image):    
+        img = img.get_fdata()
+        
+    if isinstance(seg, torch.Tensor):
+        seg= seg[0].cpu().numpy().astype(np.uint8)
+   
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(3 * 4, 4))
+    axes[0].imshow(img[ img.shape[0] // 2 + delta, :, :])
+    axes[1].imshow(seg[ seg.shape[0] // 2 + delta, :, :])
+    intersect = img[ img.shape[0] // 2 + delta, :, :] + seg[ seg.shape[0] // 2 + delta, :, :]*100
+    axes[2].imshow(intersect, cmap='gray')
+    
+    plt.show()
